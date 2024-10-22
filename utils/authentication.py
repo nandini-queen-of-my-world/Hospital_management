71d0f7fd700ae65ea73592676a6bf39628bd1409
Nandini <120407395+nandini-queen-of-my-world@users.noreply.github.com>
2024-10-22 21:10:47 +0530

    Update authentication.py

--- utils/authentication.py
+++ utils/authentication.py
@@ -1,12 +1,10 @@
-import streamlit as st
-
 def authenticate_user(role, password):
     credentials = {
-        'Admin': st.secrets['general']['ADMIN_PASSWORD'],
-        'Receptionist': st.secrets['general']['RECEPTIONIST_PASSWORD'],
-        'Head Nurse': st.secrets['general']['HEAD_NURSE_PASSWORD'],
-        'Accountant': st.secrets['general']['ACCOUNTANT_PASSWORD'],
-        'Services Coordinator': st.secrets['general']['SERVICE_COORDINATOR_PASSWORD']
+        'Admin': 'admin123',
+        'Receptionist': 'recep123',
+        'Head Nurse': 'nurse123',
+        'Accountant': 'account123',
+        'Services Coordinator': 'service123'
     }
     
     if role in credentials and credentials[role] == password:

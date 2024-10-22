38bc249afe1ea9fb0f84adb8867c9e2b092239d4
Nandini <120407395+nandini-queen-of-my-world@users.noreply.github.com>
2024-10-22 21:20:23 +0530

    Update authentication.py

--- utils/authentication.py
+++ utils/authentication.py
@@ -1,10 +1,6 @@
 def authenticate_user(role, password):
     credentials = {
-        'Admin': 'admin123',
-        'Receptionist': 'recep123',
-        'Head Nurse': 'nurse123',
-        'Accountant': 'account123',
-        'Services Coordinator': 'service123'
+        'your credentials'
     }
     
     if role in credentials and credentials[role] == password:

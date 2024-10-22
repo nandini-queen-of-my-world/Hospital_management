0ca34801a056ddaf9a0caa704b839b48d03f2b7d
Nandini <120407395+nandini-queen-of-my-world@users.noreply.github.com>
2024-10-22 21:19:27 +0530

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -4,6 +4,6 @@ def get_connection():
     return psycopg2.connect(
         host="localhost",
         database="hospital_management",
-        user="postgres",
-        password="nandini@108"
+        user="your_username",
+        password="your_password"
     )

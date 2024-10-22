27d0b81c22eb22a98b109d16b3195bd0eed59906
Nandini <120407395+nandini-queen-of-my-world@users.noreply.github.com>
2024-10-22 21:11:08 +0530

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,10 +1,9 @@
 import psycopg2
-import streamlit as st
 
 def get_connection():
     return psycopg2.connect(
-        host=st.secrets['database']['DB_HOST'],
-        database=st.secrets['database']['DB_NAME'],
-        user=st.secrets['database']['DB_USER'],
-        password=st.secrets['database']['DB_PASSWORD']
+        host="localhost",
+        database="hospital_management",
+        user="postgres",
+        password="nandini@108"
     )

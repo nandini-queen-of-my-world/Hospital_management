801dc4fc0eaef3aeff3dd77295380d2c0b4fd156
Nandini <120407395+nandini-queen-of-my-world@users.noreply.github.com>
2024-10-22 21:20:40 +0530

    Delete utils directory

--- utils/authentication.py
+++
@@ -1,8 +0,0 @@
-def authenticate_user(role, password):
-    credentials = {
-        'your credentials'
-    }
-    
-    if role in credentials and credentials[role] == password:
-        return True
-    return False
--- utils/database.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def get_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="hospital_management",
-        user="your_username",
-        password="your_password"
-    )
